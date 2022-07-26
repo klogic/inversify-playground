5a7e8e912f25114a1d0e759ff6733fae01d0ab3d
narongsak keawmanee <klogic@hotmail.co.th>
2022-07-26 22:44:11 +0700

    feat: 🎸 initial commit with simple inversify

---
+++ inversify.config.ts
@@ -0,0 +1,12 @@
+import { Container } from "inversify";
+import "reflect-metadata";
+import { TYPES } from "./types";
+import { Warrior, ThrowableWeapon, Weapon } from "./interfaces";
+import { Ninja, Katana, Shuriken } from "./entities";
+
+const myContainer = new Container();
+myContainer.bind<Warrior>(TYPES.Warrior).to(Ninja);
+myContainer.bind<Weapon>(TYPES.Weapon).to(Katana);
+myContainer.bind<ThrowableWeapon>(TYPES.ThrowableWeapon).to(Shuriken);
+
+export { myContainer };
