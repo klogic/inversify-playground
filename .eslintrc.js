e9f2c01fc3e471e8997e689c38c3b0cb86bf3762
narongsak keawmanee <klogic@hotmail.co.th>
2022-07-28 16:40:28 +0700

    refactor: adjust the rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    rules: {
-        indent: "error",
-        "no-return-await": "error",
-        "no-return-await": "error",
-        "require-await": "error",
-        "jsx-quotes": "error",
-        "no-trailing-spaces": "error",
-        quotes: "error",
-        semi: ["error", "always"],
-    },
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    indent: ["error", 2],
+    "no-return-await": "error",
+    "no-return-await": "error",
+    "require-await": "error",
+    "jsx-quotes": "error",
+    "no-trailing-spaces": "error",
+    quotes: "error",
+    semi: ["error", "always"],
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
 };
